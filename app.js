7511adf6fa3e2c457cf32aabc5a7f728738bb56d
samoody2 <69129144+samoody2@users.noreply.github.com>
2021-04-19 11:12:07 -0500

    Updated app with basic functionality
    different from original html, css and js files

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-window.addEventListener('load', ()=> {
-    let long;
-    let lat;
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue');
+var ame = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            
-            
-        })
-    } else{
-        h1.textContent = "Please reload and allow geolocation to use app"
-    }
-});
+button.addEventListener('click',function(){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=`+inputValue.value+`&appid=bbdb631e430c224015c0a59c0ac37d9e&units=imperial`)
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+
+        ame.innerHTML = nameValue;
+        temp.innerHTML = tempValue);
+        desc.innerHTML = descValue;
+
+    })
+    
+    nameValue
+
+.catch(err => alert("Wrong Zipcode."))
+
+})
+
